8767f53152b974b20197100518d654c0bdc550e5
Nick Ivanov <n.ivanov@auckland.ac.nz>
2018-11-30 11:48:38 +1300

    GROUPER-387: create-initial-swagger-definition
    Saved change to application class

--- grouper-ws/grouper-ws/src/grouper-ws/edu/internet2/middleware/grouper/ws/GrouperWSApplication.java
+++ grouper-ws/grouper-ws/src/grouper-ws/edu/internet2/middleware/grouper/ws/GrouperWSApplication.java
@@ -2,13 +2,29 @@ package edu.internet2.middleware.grouper.ws;
 
 import javax.ws.rs.ApplicationPath;
 
-import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ *
+ */
 @ApplicationPath("/*")
-public class MyApplication extends ResourceConfig {
+@SpringBootApplication
+public class GrouperWSApplication extends SpringBootServletInitializer { // extends ResourceConfig {
 
-    public MyApplication() {
+  public GrouperWSApplication() {
     //public MyApplication(@Context ServletConfig servletConfig) {
-        super();
-    }
+    super();
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(GrouperWSApplication.class);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(GrouperWSApplication.class, args);
+  }
 }
