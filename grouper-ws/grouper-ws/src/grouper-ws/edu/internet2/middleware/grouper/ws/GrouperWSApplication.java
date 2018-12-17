9a2bcde8e0d89f5dcc8347c55d95cfb82019a6af
Nick Ivanov <n.ivanov@auckland.ac.nz>
2018-12-18 10:28:23 +1300

    GROUPER-389: swagger definition Saved changes

--- grouper-ws/grouper-ws/src/grouper-ws/edu/internet2/middleware/grouper/ws/GrouperWSApplication.java
+++ grouper-ws/grouper-ws/src/grouper-ws/edu/internet2/middleware/grouper/ws/GrouperWSApplication.java
@@ -1,28 +1,33 @@
 package edu.internet2.middleware.grouper.ws;
 
-import javax.ws.rs.ApplicationPath;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  */
-@ApplicationPath("/*")
+// @ApplicationPath("/*")
+@Configuration
 @SpringBootApplication
-public class GrouperWSApplication extends SpringBootServletInitializer { // extends ResourceConfig {
+@ComponentScan(basePackages = {
+    "edu.internet2" } /*, excludeFilters = @Filter({
+                      RestController.class, Configuration.class })*/
+)
+public class GrouperWSApplication { // extends SpringBootServletInitializer { // extends ResourceConfig {
 
   public GrouperWSApplication() {
     //public MyApplication(@Context ServletConfig servletConfig) {
     super();
   }
 
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(GrouperWSApplication.class);
-  }
+  //  @Override
+  //  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+  //    return application.sources(GrouperWSApplication.class);
+  //  }
 
   public static void main(String[] args) {
     SpringApplication.run(GrouperWSApplication.class, args);
